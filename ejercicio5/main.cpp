9be4d5f3970c29907061dac997f3c04fbb8c7a55
josecarlos0112 <jzorrgar@myuax.com>
2023-12-12 12:48:53 +0100

    Subida de quinto ejercicio

---
+++ ejercicio5/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class Estudiante {
+private:
+    std::string nombre;
+    int edad;
+    float promedio;
+
+public:
+    Estudiante(std::string n, int e, float p) : nombre(n), edad(e), promedio(p) {}
+
+    void imprimir() {
+        std::cout << "Nombre: " << nombre << ", Edad: " << edad << ", Promedio: " << promedio << std::endl;
+    }
+
+    void modificarEdad(int nuevaEdad) {
+        edad = nuevaEdad;
+    }
+};
+
+int main() {
+    Estudiante estudiante1("Jose", 20, 95.5);
+    estudiante1.imprimir();
+
+    estudiante1.modificarEdad(30);
+    estudiante1.imprimir();
+
+    return 0;
+}
